ab9b803cdeaa9690aa91fdaf49e059b59b4e2ada
kiran Narute <kirannarute486@gmail.com>
2025-11-19 17:07:27 +0530

    Switched to Render PostgreSQL

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ load_dotenv()
 app.secret_key = os.getenv("SECRET_KEY")
 
 DB_CONFIG = {
-    "dbname": os.getenv("DB_NAME"),
+    "host": os.getenv("DB_HOST"),
+    "database": os.getenv("DB_NAME"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT")
+    "port": os.getenv("DB_PORT", 5432)
 }
 
 
